19ccaaaa1c1f20e4aa8ff534c4d006cd85b29a98
Aon <FinalDipThank@gmail.com>
2022-08-03 22:17:51 +0700

    delete welcome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-      { path: '', pathMatch: 'full', redirectTo: 'welcome' },
-      { path: 'welcome', component: PortfoilioComponent },
-      { path: '**', redirectTo: '/welcome' }
+      { path: '', component: PortfoilioComponent }
 ];
 
 @NgModule({
